93a7b7d51589c0ff950d567d457f1d5f0109c7c8
Roman <romaaverkin@gmail.com>
2022-07-14 15:58:28 +0700

    Наследование шаблонов

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,17 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Объявления</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
-          integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
-</head>
-<body>
-<div class="container">
-    <h1 class="my-3 text-center">Объявления</h1>
+@extends('layouts.base')
+
+@section('title', 'Главная')
+
+@section('main')
     @if (count($bbs) > 0)
         <table class="table table-striped">
             <thead>
@@ -27,16 +18,11 @@
                     <td><h3>{{ $bb->title }}</h3></td>
                     <td>{{ $bb->price }}</td>
                     <td>
-                        <a href="/{{ $bb->id }}">Подробнее..</a>
+                        <a href="/{{ $bb->id }}/">Подробнее..</a>
                     </td>
                 </tr>
             @endforeach
             </tbody>
         </table>
     @endif
-</div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
-        crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection('main')
