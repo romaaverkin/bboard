b01380b044426243996c65d449a13a8cd80f31e9
Roman <romaaverkin@gmail.com>
2022-07-18 12:26:28 +0700

    Добавил модуль авторизации

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [BbsController::class, 'index'])->name('index');
 Route::get('/{bb}', [BbsController::class, 'detail'])->name('detail');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
